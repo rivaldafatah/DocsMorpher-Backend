46ea8cd21a4dab4050615683d78369f413f2f02b
Rivalda Fatah Rachman Hernawan <rivaldarafalrachman2016@gmail.com>
2023-06-19 20:21:21 +0700

    Update Port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 6000;
+const port = 7000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 const storage = multer.diskStorage({
